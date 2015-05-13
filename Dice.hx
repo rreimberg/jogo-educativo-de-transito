1e64112efe795cc6de5ce1dbfaed252fc14dcb58
Rafael Reimberg <rreimberg@gmail.com>
2015-05-12 23:03:52 -0300

    Create Dice class and with roll method.

---
+++ Dice.hx
@@ -0,0 +1,21 @@
+import Math;
+
+class Dice {
+
+    var sides : Int;
+
+    public function new(sides) {
+        this.sides = sides;
+    }
+
+    public function roll() {
+        var number = Math.round((Math.random() * 1000) % this.sides);
+        if (number == 0) {
+            number = 1;
+        }
+
+        return number;
+    }
+
+}
+
