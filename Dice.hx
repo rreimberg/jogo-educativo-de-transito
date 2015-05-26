764ec1797b8db5a3f3e8fbfa111b323624f1092d
Rafael Reimberg <rreimberg@gmail.com>
2015-05-26 03:58:17 -0300

    Use dice images.

--- Dice.hx
+++ Dice.hx
@@ -1,3 +1,5 @@
+import flash.display.Bitmap;
+
 import Math;
 import Utils;
 
@@ -6,9 +8,20 @@ class Dice {
 
     var sides : Int;
     var value : Int;
+    var images : Array<Bitmap>;
 
     public function new(sides) {
         this.sides = sides;
+
+        this.value = 1;
+
+        this.images = new Array<Bitmap>();
+        this.images.push(new Bitmap(new ImagesResources.Dice1BitmapData(0, 0)));
+        this.images.push(new Bitmap(new ImagesResources.Dice2BitmapData(0, 0)));
+        this.images.push(new Bitmap(new ImagesResources.Dice3BitmapData(0, 0)));
+        this.images.push(new Bitmap(new ImagesResources.Dice4BitmapData(0, 0)));
+        this.images.push(new Bitmap(new ImagesResources.Dice5BitmapData(0, 0)));
+        this.images.push(new Bitmap(new ImagesResources.Dice6BitmapData(0, 0)));
     }
 
     public function roll() {
@@ -20,5 +33,8 @@ class Dice {
         return this.value;
     }
 
+    public function getImage() {
+        return this.images[this.value - 1];
+    }
 }
 
